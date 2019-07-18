2d9967b53c1f0dceca59912a53cb9673b4716225
josue kula <koolmx9@gmail.com>
2019-07-18 16:46:15 -0500

    wait for form to load input

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -2,7 +2,7 @@ from django.core import mail
 from selenium.webdriver.common.keys import Keys
 import re
 
-from .base import FunctionalTest
+from .base import FunctionalTest, wait
 
 TEST_EMAIL = 'edith@example.com'
 SUBJECT = 'Your login link for superlists'
@@ -10,16 +10,18 @@ SUBJECT = 'Your login link for superlists'
 
 class LoginTest(FunctionalTest):
 
+    @wait
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
         # It's telling her to enter her email address, so she does
 
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.wait_for(lambda: self.browser.find_element_by_name('email').send_keys(TEST_EMAIL))
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent
+
         self.wait_for(lambda: self.assertIn(
             'Check your email',
             self.browser.find_element_by_tag_name('body').text
