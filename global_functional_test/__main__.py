c9f18f07523206c9a49d8e87f9105b294fb54d8c
josue kula <koolmx9@gmail.com>
2019-07-18 15:39:36 -0500

    Trying to create a global_functional_test folder for CLI test-3

--- global_functional_test/__main__.py
+++ global_functional_test/__main__.py
@@ -2,6 +2,6 @@ import sys, os, django
 import unittest
 
 if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlist.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "..superlist.settings")
     django.setup()
     unittest.main(warnings='ignore')
