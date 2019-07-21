9f74c9a2e1c269284558a438f83bba0b2589227c
josue kula <koolmx9@gmail.com>
2019-07-21 14:07:09 -0500

    Priting user after session creation

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -19,7 +19,7 @@ class MyListsTest(FunctionalTest):
 
         # To set a cookie we need to first visit the domain.
         # 404 pages load the quickest!
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
             value=session_key,
