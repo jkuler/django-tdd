f710c26051e36b9b66f941f7e1d64544a6c61c7c
josue kula <koolmx9@gmail.com>
2019-07-21 13:01:12 -0500

    changing returning modied_fn()

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -21,7 +21,7 @@ def wait(fn):
                     raise e
                 time.sleep(5)
 
-    return modified_fn
+    return modified_fn()
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -50,7 +50,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, email):
-        self.browser.find_element_by_id('id_logout')
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
