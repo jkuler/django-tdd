bf2acc9ffb6f654f412c8e02f1acfa733f03e3b4
josue kula <koolmx9@gmail.com>
2019-07-21 18:47:49 -0500

    removing password at server_tools and increasing the wait time

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -19,7 +19,7 @@ def wait(fn):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-                time.sleep(0.5)
+                time.sleep(5)
 
     return modified_fn
 
