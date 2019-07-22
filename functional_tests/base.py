bbcd943612fb5fb29c27313055164bfc4425ce12
josue kula <koolmx9@gmail.com>
2019-07-21 23:06:25 -0500

    bring back the YAHOO_POP3

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -48,10 +48,9 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
