7212e0d4d19ac628e6788c05ea470b2dde5946ab
josue kula <koolmx9@gmail.com>
2019-07-21 23:25:49 -0500

    using add_list_item

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -49,6 +49,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         return self.browser.find_element_by_id('id_text')
 
     @wait
+    def get_table_rows(self):
+        return self.browser.find_elements_by_css_selector('#id_list_table tr')
+
+    @wait
     def wait_to_be_logged_in(self, email):
         self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
@@ -60,9 +64,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
 
-    @wait
     def add_list_item(self, item_text):
-        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
+        num_rows = len(self.get_table_rows())
         self.get_item_input_box().send_keys(item_text)
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -20,7 +20,7 @@ class ItemValidationTest(FunctionalTest):
             '#id_text:invalid'
         ))
         # she tries again with some text for the item, which now works
-        self.add_list_item('Buy milk')
+        self.get_item_input_box().send_keys('Buy milk')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
@@ -32,7 +32,7 @@ class ItemValidationTest(FunctionalTest):
             '#id_text:invalid'
         ))
         # And she can correct it by filling some text in
-        self.add_list_item('Make tea')
+        self.get_item_input_box().send_keys('Make tea')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
@@ -40,12 +40,12 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_duplicate_items(self):
         # Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
-        self.add_list_item('Buy wellies')
+        self.get_item_input_box().send_keys('Buy wellies')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy wellies')
 
         # She accidentally tries to enter a duplicate item
-        self.add_list_item('Buy wellies')
+        self.get_item_input_box().send_keys('Buy wellies')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She sees a helpful error message
@@ -57,10 +57,10 @@ class ItemValidationTest(FunctionalTest):
     def test_error_messages_are_cleared_on_input(self):
         # Edith starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
-        self.add_list_item('Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Banter too thick')
-        self.add_list_item('Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
