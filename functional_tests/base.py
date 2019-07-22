6a6a0c0a52b9f8c95df7a231ee98cf6f8cddae9e
josue kula <koolmx9@gmail.com>
2019-07-21 21:33:37 -0500

    using wait_for at base.py: functional_Tests

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -62,13 +62,13 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.assertNotIn(email, navbar.text)
 
     def add_list_item(self, item_text):
-        num_rows = self.wait_for(
+        num_rows = len(self.wait_for(
             lambda: self.browser.find_elements_by_css_selector('#id_list_table tr')
-        )
+        ))
 
         self.get_item_input_box().send_keys(item_text)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        item_number = len(num_rows) + 1
+        item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
 
